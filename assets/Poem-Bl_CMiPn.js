bb89a41c203e1c672c816da1b2c7e0e547b673c7
as6797779 <as6797779@users.noreply.github.com>
2026-02-27 10:17:11 +0000

    deploy: 7530ed7896a6a730374d19ce688c57db8bcdcad8

---
+++ assets/Poem-Bl_CMiPn.js
@@ -0,0 +1,11 @@
+import{_ as s,a as r,b as e,o as a}from"./index-D51SBLY5.js";const _={__name:"Poem",setup(o,{expose:n}){n();const t={};return Object.defineProperty(t,"__isScriptSetup",{enumerable:!1,value:!0}),t}},i={class:"writing-poem"};function p(o,n,t,c,u,l){return a(),r("div",i,[...n[0]||(n[0]=[e("h1",null,"📜 写作广场 - 诗歌创作",-1),e("div",{class:"poem-content"},[e("pre",null,`《夜》
+星光落进窗沿，
+晚风轻叩门帘，
+笔尖划过纸页，
+心事藏进字间。
+      `),e("div",{class:"poem-item"},[e("pre",null,`《晨》
+朝阳吻醒露水，
+鸟鸣穿过林间，
+新的一天启程，
+步履奔赴向前。
+        `)])],-1)])])}const d=s(_,[["render",p],["__scopeId","data-v-e70bff49"],["__file","/home/runner/work/as6797779.github.io/as6797779.github.io/src/views/Writing/Poem.vue"]]);export{d as default};
--- assets/index-ZF8wQ9IP.js
+++
@@ -1,1 +0,0 @@
-import{c as o,r,o as t}from"./index-DrZ1O0hA.js";const a={__name:"index",setup(n){return(c,_)=>{const e=r("router-view");return t(),o(e)}}};export{a as default};
